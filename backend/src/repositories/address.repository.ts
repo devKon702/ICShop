0ae3f8d0c61edffe3cfa70dba8a0cbbfd25dc14d
Nguyen Nhat Kha <nhatkha117@gmail.com>
2025-10-03 16:08:46 +0700

    fix(be): rewrite response data form remote api

--- backend/src/repositories/address.repository.ts
+++ backend/src/repositories/address.repository.ts
@@ -1,6 +1,8 @@
 import { prisma } from "../prisma";
 
 class AddressRepository {
+  private readonly remoteApi = "https://provinces.open-api.vn/api/v1";
+
   public findByUserId = async (userId: number) => {
     return prisma.address.findMany({
       where: { userId },
@@ -51,46 +53,54 @@ class AddressRepository {
   };
 
   public findAllProvinces = async () => {
-    return fetch("https://provinces.open-api.vn/api/p/").then(
-      (res) => res.json() as Promise<{ name: string; code: number }[]>
-    );
+    return fetch(`${this.remoteApi}/p/`)
+      .then((res) => res.json() as Promise<{ name: string; code: number }[]>)
+      .then((data) =>
+        data.map((province) => ({ name: province.name, code: province.code }))
+      )
+      .catch(() => null);
   };
 
   public findDistrictsByProvinceCode = async (provinceCode: number) => {
-    return fetch(`https://provinces.open-api.vn/api/p/${provinceCode}?depth=2`)
+    return fetch(`${this.remoteApi}/p/${provinceCode}?depth=2`)
       .then(
         (res) =>
           res.json() as Promise<{
             districts: { name: string; code: number }[];
           }>
       )
-      .then((data) => data.districts)
+      .then((data) =>
+        data.districts.map((d) => ({ name: d.name, code: d.code }))
+      )
       .catch(() => null);
   };
 
   public findCommunesByDistrictCode = async (districtCode: number) => {
-    return fetch(`https://provinces.open-api.vn/api/d/${districtCode}?depth=2`)
+    return fetch(`${this.remoteApi}/d/${districtCode}?depth=2`)
       .then(
         (res) =>
           res.json() as Promise<{ wards: { name: string; code: number }[] }>
       )
-      .then((data) => data.wards)
+      .then((data) => data.wards.map((w) => ({ name: w.name, code: w.code })))
       .catch(() => null);
   };
 
   public findProvinceByCode = async (provinceCode: number) => {
-    return fetch(`https://provinces.open-api.vn/api/p/${provinceCode}`)
+    return fetch(`${this.remoteApi}/p/${provinceCode}`)
       .then((res) => res.json() as Promise<{ name: string; code: number }>)
+      .then((data) => ({ name: data.name, code: data.code }))
       .catch(() => null);
   };
   public findDistrictByCode = async (districtCode: number) => {
-    return fetch(`https://provinces.open-api.vn/api/d/${districtCode}`)
+    return fetch(`${this.remoteApi}/d/${districtCode}`)
       .then((res) => res.json() as Promise<{ name: string; code: number }>)
+      .then((data) => ({ name: data.name, code: data.code }))
       .catch(() => null);
   };
   public findCommuneByCode = async (communeCode: number) => {
-    return fetch(`https://provinces.open-api.vn/api/w/${communeCode}`)
+    return fetch(`${this.remoteApi}/w/${communeCode}`)
       .then((res) => res.json() as Promise<{ name: string; code: number }>)
+      .then((data) => ({ name: data.name, code: data.code }))
       .catch(() => null);
   };
 }
